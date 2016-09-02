ce44da29c1b5bb4c6134e7a044519a58340ddb44
tfennelly <tom.fennelly@gmail.com>
2016-09-02 14:39:56 +0100

    Use JDL theme

--- website/javascripts/main.js
+++ website/javascripts/main.js
@@ -1,1 +1,19 @@
-console.log('This would be the main JS file.');
+(function($){
+    $(document).ready(function() {
+
+        // Set the size of the JDL storybook frame.
+        var jdlStorybookFrame = $('.jdl-storybook');
+        if (jdlStorybookFrame.length === 1) {
+            var $window = $(window);
+
+            function setStoryBookFrameHeith() {
+                var offset = jdlStorybookFrame.offset();
+                jdlStorybookFrame.height($window.height() - offset.top - 20);
+            }
+            $window.resize(setStoryBookFrameHeith);
+
+            setStoryBookFrameHeith();
+        }
+
+    });
+}(jQuery));
