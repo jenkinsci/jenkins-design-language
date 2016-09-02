f131b04514c7228ab5bd3fbbfd3a65df6385f8ff
tfennelly <tom.fennelly@gmail.com>
2016-09-02 15:09:28 +0100

    Style the nav buttons using JDL

--- website/javascripts/main.js
+++ website/javascripts/main.js
@@ -15,5 +15,18 @@
             setStoryBookFrameHeith();
         }
 
+        // Style the nav buttons using JDL styles
+        var currentUrl = window.location.href;
+        $('.nav a').addClass('btn-secondary inverse').each(function() {
+            var a = $(this);
+            var ahref = a.attr('href');
+
+            if (ahref.indexOf('./') === 0) {
+                ahref = ahref.substring(1);
+            }
+            if (currentUrl.length > ahref.length && currentUrl.substring(currentUrl.length - ahref.length) === ahref) {
+                a.removeClass('btn-secondary').addClass('btn');
+            }
+        });
     });
 }(jQuery));
