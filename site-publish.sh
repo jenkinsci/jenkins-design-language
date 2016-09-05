a2d189c7388e295c8b6d92ea86bcecb81f2b567f
tfennelly <tom.fennelly@gmail.com>
2016-09-05 10:10:09 +0100

    Publish to jenkinsci fork

--- site-publish.sh
+++ site-publish.sh
@@ -7,8 +7,7 @@ npm run site-build
 pushd website-build
 
 git init
-# TODO: point this back at the upstream repo
-git remote add webpages git@github.com:tfennelly/jenkins-design-language.git
+git remote add webpages git@github.com:jenkinsci/jenkins-design-language.git
 git fetch --depth=1 webpages gh-pages
 
 git add --all
