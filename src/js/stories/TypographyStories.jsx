82f6f19d5e916720f6953d7df50bd981deea8fd4
Cliff Meyers <cmeyers@cloudbees.com>
2016-11-21 11:22:28 -0500

    [JENKINS-39057] add a story for debugging text appearance / layout more easily

--- src/js/stories/TypographyStories.jsx
+++ src/js/stories/TypographyStories.jsx
@@ -1,31 +1,40 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 import {storiesOf} from '@kadira/storybook';
 import {Table} from '../components';
 
 storiesOf('Typography', module)
-    .add('Everything', everything)
-    .add('Headings', headings)
-    .add('Body', bodyText)
-    .add('Buttons', buttons)
-    .add('Table', table)
+    .add('Headings', () => <HeadingDemo />)
+    .add('Body', () => <TextDemo />)
+    .add('Buttons', () => <ButtonDemo />)
+    .add('Table', () => <TableDemo />)
+    .add('Debug', () => <Debug />)
 ;
 
-function headings() {
+function HeadingDemo(props) {
+    const styles = Object.assign({}, props.styles);
+    styles.border = '1px solid';
+
     return (
         <div>
-            <h1>Heading 1</h1>
-            <h2>Heading 2</h2>
-            <h3>Heading 3</h3>
-            <h4>Heading 4</h4>
-            <h5>Heading 5</h5>
-            <h6>Heading 6</h6>
+            <h1 style={styles}>Heading 1</h1>
+            <h2 style={styles}>Heading 2</h2>
+            <h3 style={styles}>Heading 3</h3>
+            <h4 style={styles}>Heading 4</h4>
+            <h5 style={styles}>Heading 5</h5>
+            <h6 style={styles}>Heading 6</h6>
         </div>
     );
 }
 
-function bodyText() {
+HeadingDemo.propTypes = {
+    styles: PropTypes.object,
+};
+
+function TextDemo(props) {
+    const styles = Object.assign({}, props.styles);
+    styles.border = '1px solid';
     return (
-        <p>
+        <p style={styles}>
             The quick brown fox jumps over the lazy dog.
             The planet <i>Neptune</i> is blue and <i><strong>tilted</strong></i>
             on it's side by an <strong>ancient calamity</strong>.
@@ -33,17 +42,27 @@ function bodyText() {
     );
 }
 
-function buttons() {
+TextDemo.propTypes = {
+    styles: PropTypes.object,
+};
+
+function ButtonDemo(props) {
+    const styles = Object.assign({}, props.styles);
+
     return (
         <div>
-            <button>Button</button>
+            <button style={styles}>Button</button>
             &nbsp;
-            <a className="btn">Link Button</a>
+            <a className="btn" style={styles}>Link Button</a>
         </div>
     );
 }
 
-function table() {
+ButtonDemo.propTypes = {
+    styles: PropTypes.object,
+};
+
+function TableDemo(props) {
     return (
         <Table headers={['Header', 'Column']}>
             <tr>
@@ -54,13 +73,36 @@ function table() {
     );
 }
 
-function everything() {
+function Debug() {
+    const sansSerifStyles = {
+        fontFamily: 'sans-serif',
+    };
+
+    const whateverStyles = {
+        fontFamily: 'serif',
+    };
+
     return (
-        <div>
-            {headings()}
-            {bodyText()}
-            {buttons()}
-            {table()}
+        <div style={{display: 'flex'}}>
+            <Everything title="default"/>
+            <Everything title="sans-serif" styles={sansSerifStyles} />
+            <Everything title="serif" styles={whateverStyles} />
         </div>
     );
 }
+
+function Everything(props) {
+    return (
+        <div style={{padding: 5}}>
+            <div>{props.title}</div>
+            <HeadingDemo styles={props.styles}/>
+            <TextDemo styles={props.styles}/>
+            <ButtonDemo styles={props.styles}/>
+        </div>
+    );
+}
+
+Everything.propTypes = {
+    title: PropTypes.string,
+    styles: PropTypes.object,
+};
