5ddd2c32dd8bcfd56917e14c76ef6d6ba1fa3a8a
Cliff Meyers <cmeyers@cloudbees.com>
2017-01-12 16:28:16 -0500

    [JENKINS-40956] hide the header or entire FormElement if there's no content to show

--- src/js/components/forms/FormElement.jsx
+++ src/js/components/forms/FormElement.jsx
@@ -2,6 +2,10 @@ import React, { PropTypes } from 'react';
 import { ErrorMessage } from '../ErrorMessage';
 
 export function FormElement(props) {
+    if (!props.title && !props.errorMessage && !props.children) {
+        return null;
+    }
+
     const extraClass = props.className || '';
     const errorClass = props.errorMessage ? 'u-error-state' : '';
     const dividerClass = props.showDivider ? 'u-show-divider' : '';
@@ -9,16 +13,24 @@ export function FormElement(props) {
 
     return (
         <div className={`FormElement ${extraClass} ${errorClass} ${dividerClass} ${layoutClass}`}>
+            { (props.title || props.errorMessage) &&
             <div className="FormElement-heading">
+                { props.title &&
                 <label className="FormElement-title">{props.title}</label>
-                <span>&nbsp;</span>
+                }
+                { (props.title && props.errorMessage) &&
+                <span>&nbsp;-&nbsp;</span>
+                }
                 { props.errorMessage &&
-                <ErrorMessage>- {props.errorMessage}</ErrorMessage>
+                <ErrorMessage>{props.errorMessage}</ErrorMessage>
                 }
             </div>
+            }
+            { props.children &&
             <div className="FormElement-children">
                 { props.children }
             </div>
+            }
         </div>
     );
 }
