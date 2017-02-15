cc316948e353e69db0b7bbae491cd05d183ac863
Josh McDonald <josh@joshmcdonald.info>
2017-02-15 18:32:14 +1000

    JENKINS-38753-multiline-label * Clean up some, fix up a few Flowtype issues

--- src/js/components/TruncatingLabel.jsx
+++ src/js/components/TruncatingLabel.jsx
@@ -1,38 +1,123 @@
 // @flow
 
-import React, { Component, PropTypes } from 'react';
+import React, {Component, PropTypes} from 'react';
 import ReactDOM from 'react-dom';
 
-type Props = *; // TODO: Actual props
-
-function debug(...rest) {
-    // TODO: Remove this
-    console.log('TL-DBG', ...rest);
-}
+//--------------------------------------
+//  Safety constants
+//--------------------------------------
 
 const MINLENGTH = 5; // Minimum size of cut-down text
 const MAXLOOPS = 50; // Max no of iterations attempting to find the correct size text
 
+//--------------------------------------
+//  Flow types
+//--------------------------------------
+
+// Render lifecycle
 const RS_MEASURE = 'measure'; // Mounted, text/props changed, measurement needed.
 const RS_FLUID = 'fluid'; // Text too big, in the process of trimming it down
 const RS_STABLE = 'stable'; // Done measuring, until props change
 
-// TODO: Docs
+export const RenderStateValues = {
+    [RS_MEASURE]: RS_MEASURE,
+    [RS_FLUID]: RS_FLUID,
+    [RS_STABLE]: RS_STABLE,
+};
+
+type RenderState = $Keys<typeof RenderStateValues>;
+
+// react.Component.props
+type Props = {
+    children?: string,
+    style?: Object,
+    className?: string
+};
+
+/**
+ * Multi-line label that will truncate with ellipses
+ *
+ * Use with a set width + height (or maxWidth / maxHeight) to get any use from it :D
+ */
 export class TruncatingLabel extends Component {
 
-    constructor(props) {
-        super(props);
-    }
+    //--------------------------------------
+    //  Component state / lifecycle
+    //--------------------------------------
+
+    props: Props
+
+    completeText: string; // Unabridged plain text content
+    innerText: string; // Current innerText of element - includes possible ellipses
+    renderState: RenderState; // Internal rendering lifecycle state
+    checkSizeRequest: number; // window.requestAnimationFrame handle
+
+    //--------------------------------------
+    //  Binary search state
+    //--------------------------------------
+
+    textCutoffLength: number; // Last count used to truncate completeText
+    longestGood: number; // Length of the longest truncated text that fits
+    shortestBad: number; // Length of the shortest truncated text that does not fit
+    loopCount: number; // to avoid infinite iteration
+
+    //--------------------------------------
+    //  React Lifecycle
+    //--------------------------------------
 
     componentWillMount() {
         this.handleProps(this.props);
     }
 
-    componentWillReceiveProps(nextProps:Props) {
+    componentWillReceiveProps(nextProps: Props) {
         this.handleProps(nextProps);
     }
 
-    handleProps(props) {
+    componentDidMount() {
+        this.invalidateSize();
+    }
+
+    componentDidUpdate() {
+        this.invalidateSize();
+    }
+
+    componentWillUnmount() {
+        if (this.checkSizeRequest) {
+            cancelAnimationFrame(this.checkSizeRequest);
+            this.checkSizeRequest = 0;
+        }
+    }
+
+    //--------------------------------------
+    //  Render
+    //--------------------------------------
+
+    render() {
+        let {
+            style = {},
+            className = ''
+        } = this.props;
+
+        style = {
+            overflow: 'hidden',
+            wordWrap: 'break-word',
+            ...style
+        };
+
+        if (this.renderState !== RS_STABLE) {
+            style.opacity = 0;
+        }
+
+        return (
+            <div style={style} className={'TruncatingLabel ' + className}>{this.innerText}</div>
+        );
+    }
+
+    //--------------------------------------
+    //  Internal Rendering Lifecycle
+    //--------------------------------------
+
+    handleProps(props: Props) {
 
         const {children = ""} = props;
 
@@ -48,7 +133,6 @@ export class TruncatingLabel extends Component {
         this.renderState = RS_MEASURE;
         this.innerText = this.completeText;
         this.loopCount = 0;
-        this.fluidTime = 0;
         this.longestGood = MINLENGTH;
         this.shortestBad = this.innerText.length;
     }
@@ -82,7 +166,7 @@ export class TruncatingLabel extends Component {
 
             if (tooBig) {
                 this.renderState = RS_FLUID;
-                
+
                 // Set initial params for binary search of length 
                 this.longestGood = MINLENGTH;
                 this.textCutoffLength = this.shortestBad = this.innerText.length;
@@ -94,9 +178,9 @@ export class TruncatingLabel extends Component {
 
         if (this.renderState === RS_FLUID) {
             this.loopCount++;
-            this.fluidTime = this.fluidTime || performance.now();
 
             const lastLength = this.textCutoffLength;
+
             let keepMeasuring;
 
             if (this.loopCount >= MAXLOOPS) {
@@ -109,7 +193,7 @@ export class TruncatingLabel extends Component {
                 // debug('Found optimum length!', lastLength);
                 keepMeasuring = false;
             } else {
-                
+
                 // Update search space
                 if (tooBig) {
                     this.shortestBad = Math.min(this.shortestBad, lastLength);
@@ -123,62 +207,15 @@ export class TruncatingLabel extends Component {
 
                 // Bypass react's render loop during the "fluid" state for performance
                 thisElement.innerText = this.innerText;
-
-                // debug('-------- tooBig', tooBig);
-                // debug('     lastLength', lastLength);
-                // debug('    longestGood', this.longestGood);
-                // debug('     textCutoff', this.textCutoffLength);
-                // debug('    shortestBad', this.shortestBad);
-
                 keepMeasuring = true;
             }
 
             if (keepMeasuring) {
                 this.invalidateSize();
             } else {
-
-                // let duration = Math.round(performance.now() - this.fluidTime);
-                // debug('checkSize() stable after', this.loopCount, 'iterations', duration, 'ms');
-
                 this.renderState = RS_STABLE;
                 this.forceUpdate(); // Re-render via react so it knows about updated alpha and final content
             }
-        }
-    }
-
-    render() {
-        let {
-            style = {},
-            className = ''
-        } = this.props;
-
-        style = {
-            overflow: 'hidden',
-            wordWrap: 'break-word',
-            ...style
-        };
-
-        if (this.renderState !== RS_STABLE) {
-            style.opacity = 0;
-        }
-
-        return (
-            <div style={style} className={'TruncatingLabel ' + className}>{this.innerText}</div>
-        );
-    }
-
-    componentDidMount() {
-        this.invalidateSize();
-    }
-
-    componentDidUpdate() {
-        this.invalidateSize();
-    }
-
-    componentWillUnmount() {
-        if (this.checkSizeRequest) {
-            cancelAnimationFrame(this.checkSizeRequest);
-            this.checkSizeRequest = 0;
         }
     }
 }
