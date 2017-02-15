2803de7075934b39d7aa69fa44fa12c1917de9bf
Josh McDonald <josh@joshmcdonald.info>
2017-02-15 17:59:22 +1000

    JENKINS-38753-multiline-label * Rejig the label algorithm completely to do a binary search for optimum length.  * Update PipelineGraph to use new label component instead of plain DIVs for labels

--- src/js/components/TruncatingLabel.jsx
+++ src/js/components/TruncatingLabel.jsx
@@ -10,6 +10,9 @@ function debug(...rest) {
     console.log('TL-DBG', ...rest);
 }
 
+const MINLENGTH = 5; // Minimum size of cut-down text
+const MAXLOOPS = 50; // Max no of iterations attempting to find the correct size text
+
 const RS_MEASURE = 'measure'; // Mounted, text/props changed, measurement needed.
 const RS_FLUID = 'fluid'; // Text too big, in the process of trimming it down
 const RS_STABLE = 'stable'; // Done measuring, until props change
@@ -22,7 +25,6 @@ export class TruncatingLabel extends Component {
     }
 
     componentWillMount() {
-        debug("------------------------- componentWillMount() -------------------------");
         this.handleProps(this.props);
     }
 
@@ -32,101 +34,151 @@ export class TruncatingLabel extends Component {
 
     handleProps(props) {
 
-        let {children} = props;
+        const {children = ""} = props;
 
-        if ( typeof children !== "string") {
-            console.warn("TruncatingLabel - Label children must be string for now", children);
-            children = "Contents must be string";
+        if (typeof children === "string") {
+            this.completeText = children;
+        } else if (children === null || children === false) {
+            this.completeText = ""; // Assume content has been boolean'd out
+        } else {
+            console.warn("TruncatingLabel - Label children must be string but is", typeof children, children);
+            this.completeText = "Contents must be string";
         }
 
         this.renderState = RS_MEASURE;
-        this.innerText = children;
+        this.innerText = this.completeText;
         this.loopCount = 0;
         this.fluidTime = 0;
+        this.longestGood = MINLENGTH;
+        this.shortestBad = this.innerText.length;
+    }
+
+    invalidateSize() {
+        if (!this.checkSizeRequest) {
+            this.checkSizeRequest = requestAnimationFrame(() => this.checkSize());
+        }
     }
 
     checkSize() {
+        this.checkSizeRequest = 0;
 
-        let element = ReactDOM.findDOMNode(this);
+        if (this.renderState === RS_STABLE) {
+            return; // Nothing to check, no more checks to schedule
+        }
+
+        let thisElement = ReactDOM.findDOMNode(this);
 
         let {
             scrollHeight,
-            clientHeight
-        } = element;
+            clientHeight,
+            scrollWidth,
+            clientWidth
+        } = thisElement;
 
-        const minLength = 5;
+        const tooBig = scrollHeight > clientHeight || scrollWidth > clientWidth;
 
-        if (this.innerText && this.innerText.length > minLength
-            && scrollHeight > clientHeight && this.loopCount < 50) {
+        if (this.renderState === RS_MEASURE) {
+            // First measurement since mount / props changed
 
-            // Text is currently too big!
+            if (tooBig) {
+                this.renderState = RS_FLUID;
+                
+                // Set initial params for binary search of length 
+                this.longestGood = MINLENGTH;
+                this.textCutoffLength = this.shortestBad = this.innerText.length;
+            } else {
+                this.renderState = RS_STABLE;
+                this.forceUpdate(); // Re-render via react so it can update the alpha
+            }
+        }
 
+        if (this.renderState === RS_FLUID) {
             this.loopCount++;
             this.fluidTime = this.fluidTime || performance.now();
-            this.renderState = RS_FLUID;
 
-            // Guess how much we're over, based on the ratio of clientHeight:scrollHeight
-            let newLength;
+            const lastLength = this.textCutoffLength;
+            let keepMeasuring;
 
-            let toRemove = Math.max(1, Math.floor((1 - clientHeight / scrollHeight) * this.innerText.length * 0.5));
+            if (this.loopCount >= MAXLOOPS) {
+                // This really shouldn't happen!
+                console.error('TruncatingLabel - TOO MANY LOOPS');
+                keepMeasuring = false;
+            } else if (lastLength <= MINLENGTH) {
+                keepMeasuring = false;
+            } else if (Math.abs(this.shortestBad - this.longestGood) < 2) {
+                // debug('Found optimum length!', lastLength);
+                keepMeasuring = false;
+            } else {
+                
+                // Update search space
+                if (tooBig) {
+                    this.shortestBad = Math.min(this.shortestBad, lastLength);
+                } else {
+                    this.longestGood = Math.max(this.longestGood, lastLength);
+                }
 
-            // debug('#' + this.loopCount);
-            // debug('  scrollHeight', scrollHeight);
-            // debug('  clientHeight', clientHeight);
-            // debug('      toRemove', toRemove);
+                // Calculate the next length and update the text
+                this.textCutoffLength = Math.floor((this.longestGood + this.shortestBad) / 2);
+                this.innerText = this.completeText.substr(0, this.textCutoffLength) + '…';
 
-            newLength = Math.max(minLength, this.innerText.length - toRemove);
-            this.innerText = this.innerText.substr(0, newLength) + '…';
+                // Bypass react's render loop during the "fluid" state for performance
+                thisElement.innerText = this.innerText;
 
-            // We're bypassing react's rendering so we don't slow things down while adjusting
-            element.innerText = this.innerText;
-            requestAnimationFrame(() => this.checkSize());
+                // debug('-------- tooBig', tooBig);
+                // debug('     lastLength', lastLength);
+                // debug('    longestGood', this.longestGood);
+                // debug('     textCutoff', this.textCutoffLength);
+                // debug('    shortestBad', this.shortestBad);
 
-        } else if (this.renderState === RS_MEASURE) {
+                keepMeasuring = true;
+            }
 
-            // First measurement, but it all fits
-            this.renderState = RS_STABLE;
-            this.forceUpdate(); // Re-render via react
+            if (keepMeasuring) {
+                this.invalidateSize();
+            } else {
 
-        } else if (this.renderState === RS_FLUID) {
+                // let duration = Math.round(performance.now() - this.fluidTime);
+                // debug('checkSize() stable after', this.loopCount, 'iterations', duration, 'ms');
 
-            // Last change to text has made it fit :)
-
-            let duration = Math.round(performance.now() - this.fluidTime);
-            debug('checkSize() stable after', this.loopCount, 'iterations', duration, 'ms');
-
-            this.renderState = RS_STABLE;
-            this.forceUpdate();
+                this.renderState = RS_STABLE;
+                this.forceUpdate(); // Re-render via react so it knows about updated alpha and final content
+            }
         }
     }
 
     render() {
-
-        // debug('render()', this.innerText);
-
         let {
             style = {},
-            className
+            className = ''
         } = this.props;
 
+        style = {
+            overflow: 'hidden',
+            wordWrap: 'break-word',
+            ...style
+        };
+
         if (this.renderState !== RS_STABLE) {
-            style.alpha = 0;
+            style.opacity = 0;
         }
 
         return (
-            <div style={style} className={className}>{this.innerText}</div>
+            <div style={style} className={'TruncatingLabel ' + className}>{this.innerText}</div>
         );
     }
 
     componentDidMount() {
-        if (this.renderState !== RS_STABLE) {
-            requestAnimationFrame(() => this.checkSize());
-        }
+        this.invalidateSize();
     }
 
     componentDidUpdate() {
-        if (this.renderState !== RS_STABLE) {
-            requestAnimationFrame(() => this.checkSize());
+        this.invalidateSize();
+    }
+
+    componentWillUnmount() {
+        if (this.checkSizeRequest) {
+            cancelAnimationFrame(this.checkSizeRequest);
+            this.checkSizeRequest = 0;
         }
     }
 }
--- src/js/stories/LabelStories.jsx
+++ src/js/stories/LabelStories.jsx
@@ -5,13 +5,54 @@ import {hgw} from './data/textdata';
 
 storiesOf('Label', module)
     .add('Basic', basic)
+    .add('Broken 1', basic2)
+    .add('Null children', empty1)
+    .add('Missing children', empty2)
+    .add('Blank children', empty3)
     .add('Many', many)
 ;
 
 function basic() {
     return (
         <div style={{margin:'1em'}}>
-            {example(210, 121)}
+            {example(210, 121, hgw)}
+        </div>
+    )
+}
+
+function empty1() {
+    return (
+        <div style={{margin:'1em'}}>
+            {example(200, 200, null)}
+        </div>
+    )
+}
+
+function empty2() {
+    return (
+        <div style={{margin:'1em'}}>
+            {example(200, 200, undefined)}
+        </div>
+    )
+}
+
+function empty3() {
+    return (
+        <div style={{margin:'1em'}}>
+            {example(200, 200, "")}
+        </div>
+    )
+}
+
+function basic2() {
+
+    // This was showing a failure to settle within 50 iterations in the pipeline graph
+
+    const badString = "Das komputermaschine ist nicht auf mittengraben unt die gerfingerpoken. Watchen das blinkenlights.";
+
+    return (
+        <div style={{margin:'1em', fontSize: '80%'}}>
+            {example(89, 34, badString)}
         </div>
     )
 }
@@ -19,22 +60,22 @@ function basic() {
 function many() {
     return (
         <div style={{margin:'1em'}}>
-            {example(100, 100)}
+            {example(100, 100, hgw)}
             <br/>
-            {example(180, 100)}
+            {example(180, 100, hgw)}
             <br/>
-            {example(200, 100)}
+            {example(200, 100, hgw)}
             <br/>
-            {example(50, 180)}
+            {example(50, 180, hgw)}
             <br/>
-            {example(80, 80)}
+            {example(80, 80, hgw)}
             <br/>
-            {example(180, 180)}
+            {example(180, 180, hgw)}
         </div>
     );
 }
 
-function example(width, height) {
+function example(width, height, txt) {
 
     const w = width + 'px';
     const h = height + 'px';
@@ -47,14 +88,13 @@ function example(width, height) {
 
     const style2 = {
         maxWidth: w,
-        maxHeight: h,
-        overflow: 'hidden'
+        maxHeight: h
     };
 
     return (
         <div style={outer}>
             <TruncatingLabel style={style2}>
-                {hgw}
+                {txt}
             </TruncatingLabel>
         </div>
     );
