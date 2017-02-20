88312d7b62083c585c33200dc2fac2227a152991
Josh McDonald <josh@joshmcdonald.info>
2017-02-20 16:22:01 +1000

    josh/clean-up-old-headers * Remove some surplus old headewr components

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -9,7 +9,7 @@ export {
 export { WeatherIcon } from './weather-icon';
 export { Page } from './page';
 export { GlobalHeader, GlobalNav } from './global-header';
-export { PageHeader, Title, PageTabsOld, TabLink } from './page-header';
+export { TabLink } from './TabLink';
 export { Table, TableDivider } from './Table';
 export {
     StatusIndicator,
