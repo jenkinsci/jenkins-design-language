d77c938caeefe036ddcf4306dd7debab37521c85
Keith Zantow <kzantow@gmail.com>
2018-06-28 16:00:10 -0400

    fix(test): cleanup

--- components/Button/Button.test.tsx
+++ components/Button/Button.test.tsx
@@ -1,13 +1,12 @@
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
-import '../Button.scss';
 import { Button } from './Button';
 
-describe('<Button />', () => {
-    it('should create and render a button with text.', () => {
+describe('Button', () => {
+    it('should create and render a button with text', () => {
         const text = 'My button.';
         const element = <Button>{text}</Button>;
         const tree = renderer.create(element).toJSON();
-        console.log('tree.children[0] ', tree.children[0]);
+        expect(tree.children[0]).toEqual(text);
     });
 });
