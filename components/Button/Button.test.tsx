721d5f2df105867baa3600fa5dc6eeff8ecf235c
Keith Zantow <kzantow@gmail.com>
2018-06-28 16:47:50 -0400

    chore(test): 100% coverage on Button

--- components/Button/Button.test.tsx
+++ components/Button/Button.test.tsx
@@ -1,12 +1,24 @@
 import * as React from 'react';
+import * as Enzyme from 'enzyme';
+import ReactTestUtils from 'react-dom/test-utils';
 import * as renderer from 'react-test-renderer';
 import { Button } from './Button';
 
 describe('Button', () => {
     it('should create and render a button with text', () => {
-        const text = 'My button.';
+        const text = 'My Button';
         const element = <Button>{text}</Button>;
         const tree = renderer.create(element).toJSON();
         expect(tree.children[0]).toEqual(text);
+    });
+
+    it('handles click events', done => {
+        const e = <Button onClick={() => done()}>asdf</Button>;
+        Enzyme.mount(e).simulate('click');
+    });
+
+    it('works without onClick', () => {
+        const e = <Button>asdf</Button>;
+        Enzyme.mount(e).simulate('click');
     });
 });
