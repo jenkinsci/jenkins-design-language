9d6bbc0e21a78537ea8773bca35592ece092e7d6
Keith Zantow <kzantow@gmail.com>
2018-06-29 00:45:15 -0400

    chore: better/much smaller docs diffs

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,4 +1,80 @@
 const path = require('path');
+
+const webpack = require('webpack');
+const beautify = require('js-beautify');
+const { RawSource } = require('webpack-sources');
+const pkg = require('./package.json');
+const crypto = require('crypto');
+
+class BeautifyPlugin {
+    constructor(config = {}) {
+        this.webpack4 =
+            webpack.version && /.*4(\.\d+){0,2}/gi.test(webpack.version);
+        this.options = {
+            config: Object.assign(
+                {
+                    indent_size: 1,
+                    indent_with_tabs: false,
+                },
+                config
+            ),
+        };
+    }
+
+    beautify(compilation) {
+        const hash = s =>
+            crypto
+                .createHash('sha1')
+                .update(s)
+                .digest('base64');
+        const hashes = {};
+        Object.keys(compilation.assets).forEach(k => {
+            const asset = compilation.assets[k];
+            if (/.*\.js/.test(k) && asset.source) {
+                const source = asset.source();
+                hashes[k] = hash(source).substring(0, 9);
+                // console.log('generated hash:', hashes[k], 'for:', k);
+                compilation.assets[k] = new RawSource(
+                    beautify(source, this.options)
+                );
+            } else if (/.*\.html/.test(k) && asset.source) {
+                const suffix = '.js?t=' + new Date().getTime();
+                // for html, cache bust with a query string
+                let source = asset.source();
+                source = source.replace(/"([^"]+?\.js)"/g, (_, match) => {
+                    // console.log(
+                    //     'replacing:',
+                    //     match,
+                    //     'with hash:',
+                    //     hashes[match]
+                    // );
+                    return '"' + match + '?v=' + hashes[match] + '"';
+                });
+                compilation.assets[k] = new RawSource(
+                    beautify.html(source, this.options)
+                );
+            } else if (/.*\.css/.test(k) && asset.source) {
+                compilation.assets[k] = new RawSource(
+                    beautify.css(asset.source(), this.options)
+                );
+            }
+        });
+    }
+
+    apply(compiler) {
+        if (this.webpack4) {
+            compiler.hooks.emit.tap('BeautifyPlugin', compilation => {
+                this.beautify(compilation);
+            });
+        } else {
+            compiler.plugin('emit', (compilation, callback) => {
+                this.beautify(compilation);
+                callback();
+            });
+        }
+    }
+}
+
 module.exports = {
     title: 'Jenkins Design Language',
     // See this: https://github.com/styleguidist/react-styleguidist/blob/master/examples/sections/styleguide.config.js
@@ -15,6 +91,27 @@ module.exports = {
     ],
     // components: 'components/**/*.tsx',
     ignore: ['**/*.stories.tsx', '**/*.tests.tsx', '**/*.test.tsx'],
+    dangerouslyUpdateWebpackConfig: function(webpackConfig, env) {
+        Object.assign(webpackConfig.output, {
+            filename: 'build/[name].js',
+            chunkFilename: 'build/[name]-[chunkhash].js',
+        });
+        webpackConfig.plugins = webpackConfig.plugins.concat([
+            new BeautifyPlugin(),
+            // Extract all 3rd party modules into a separate 'vendor' chunk
+            new webpack.optimize.LimitChunkCountPlugin({
+                maxChunks: 1,
+            }),
+            new webpack.optimize.CommonsChunkPlugin({
+                name: 'vendor',
+                minChunks: ({ resource }) => /node_modules/.test(resource),
+            }),
+            new webpack.HashedModuleIdsPlugin(),
+        ]);
+        webpackConfig.entry.vendor = Object.keys(pkg.dependencies);
+
+        return webpackConfig;
+    },
     webpackConfig: Object.assign(
         {},
         {
