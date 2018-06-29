68c06ddf73c0806d75439d88f3d8acebb9bf0780
Keith Zantow <kzantow@gmail.com>
2018-06-29 11:38:02 -0400

    Remove unecessary facade for storybook / test integration

--- .storybook/config.js
+++ .storybook/config.js
@@ -7,6 +7,15 @@ const Adapter = require('enzyme-adapter-react-16');
 
 Enzyme.configure({ adapter: new Adapter() });
 
+const addon = require('storybook-addon-specifications');
+const { specs, describe, it } = addon;
+
+window.specs = specs;
+window.describe = describe;
+window.it = it;
+
+// BELOW CAME FROM STORYBOOK --------------
+
 const { configure } = require('@storybook/react');
 
 // automatically import all files ending in *.stories.js
--- components/Button/Button.stories.tsx
+++ components/Button/Button.stories.tsx
@@ -2,12 +2,12 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Button } from './Button';
 
-const stories = storiesOf('Button', module);
+declare var specs: any;
 
-const specs = require('../../.storybook/facade').specs;
+const stories = storiesOf('Button', module);
 
 stories.add('should display text', () => {
     const story = <Button>some-text</Button>;
-    specs(() => require('./Button.test.tsx').default);
+    specs(() => require('./Button.test.tsx') && 'Button');
     return story;
 });
