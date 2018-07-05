10e0d8f29b11b24b601671ddf182833d4adc6ba7
lausena <lau.sena.gabriel@gmail.com>
2018-07-05 12:01:17 -0400

    Removing unneeded props and refactoring

--- components/Dialog/Dialog.tsx
+++ components/Dialog/Dialog.tsx
@@ -7,14 +7,12 @@ import { observable } from 'mobx';
 export type Props = ButtonActionProps & ButtonComponentProps;
 
 export type ButtonActionProps = Partial<{
-    readonly onDismiss: () => void;
     readonly shouldCloseOnEsc: boolean;
     store: any;
 }>;
 
 export type ButtonComponentProps = Partial<{
     readonly title: string;
-    readonly className: string;
     children: JSX.Element;
 }>;
 
@@ -22,10 +20,10 @@ export type ButtonComponentProps = Partial<{
 export class Dialog extends React.Component<Props> {
     render() {
         const store = this.props.store;
-        // TODO - shouldCloseOnEsc: should close one esc is not working as intended.
+        const shouldCloseOnEsc = this.props.hasOwnProperty('shouldCloseOnEsc') ? this.props.shouldCloseOnEsc : true;
         const defaultProps = {
             overlayClassName: 'Dialog-Overlay',
-            onRequestClose: store.closeDialog,
+            shouldCloseOnEsc
         };
         return (
             <>
@@ -33,11 +31,13 @@ export class Dialog extends React.Component<Props> {
                     <ReactModal
                         {...defaultProps}
                         className={classNames('Dialog', dialog.className)}
+                        onRequestClose={() => store.closeDialog(idx)}
                         isOpen={store.getCurrentDialogState(idx)}
                         key={idx}
                         ariaHideApp={false}
+                        shouldCloseOnEsc
                     >
-                        {dialog.title && <DialogHeader title={dialog.title} />}
+                        {dialog.title && <DialogHeader title={dialog.title}/>}
                         <DialogContent>{dialog.children}</DialogContent>
                         <DialogButtons>
                             <div className="Button" onClick={() => store.closeDialog(idx)}>
@@ -48,39 +48,6 @@ export class Dialog extends React.Component<Props> {
                 ))}
             </>
         );
-    }
-}
-
-export class DialogActions {
-    render() {
-        return <Dialog store={dialogs} />;
-    }
-}
-
-export class DialogManager extends DialogActions {
-    @observable showDialog = true;
-
-    @observable dialogs: any[] = [];
-
-    closeDialog(idx: number) {
-        this.dialogs[idx].showDialog = false;
-        if (this.dialogs[idx].onDismiss) {
-            this.dialogs[idx].onDismiss();
-        }
-    }
-
-    getCurrentDialogState(idx: number): boolean {
-        return this.dialogs[idx].showDialog;
-    }
-
-    addDialog(title: string, className: string, children: JSX.Element, onDismiss?: () => void) {
-        this.dialogs.push({
-            title: title,
-            className: className,
-            children: children,
-            showDialog: this.showDialog,
-            onDismiss: onDismiss,
-        });
     }
 }
 
@@ -114,6 +81,39 @@ export interface DialogButtonsProps {
 
 export function DialogButtons(props: DialogButtonsProps) {
     return <div className="Dialog-button-bar">{props.children}</div>;
+}
+
+export class DialogActions {
+    render() {
+        return <Dialog store={dialogs}/>;
+    }
+}
+
+export class DialogManager extends DialogActions {
+    @observable showDialog = true;
+
+    @observable dialogs: any[] = [];
+
+    closeDialog(idx: number) {
+        this.dialogs[idx].showDialog = false;
+        if (this.dialogs[idx].onDismiss) {
+            this.dialogs[idx].onDismiss();
+        }
+    }
+
+    getCurrentDialogState(idx: number): boolean {
+        return this.dialogs[idx].showDialog;
+    }
+
+    addDialog(title: string, className: string, children: JSX.Element, onDismiss?: () => void) {
+        this.dialogs.push({
+            title: title,
+            className: className,
+            children: children,
+            showDialog: this.showDialog,
+            onDismiss: onDismiss,
+        });
+    }
 }
 
 export const dialogs = new DialogManager();
