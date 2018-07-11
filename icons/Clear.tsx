840a3672e516e3d4618dcd0f083b55bd03311c8d
lausena <lau.sena.gabriel@gmail.com>
2018-07-11 16:02:40 -0400

    clear icon

--- icons/Clear.tsx
+++ icons/Clear.tsx
@@ -1,27 +1,27 @@
 import * as React from 'react';
 import { IconProps } from './IconProps';
-export function Clear({ className, size = 18, width = size, height = size }: IconProps) {
+export function Clear({ className, size = 16, width = size, height = size }: IconProps) {
     return (
         <svg
             width={width}
             height={height}
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
+            viewBox="0 0 16 16"
             className={className}
         >
-            <g fill="none" clipPath="url(#icon24pxClearClip0)">
-                <path
-                    d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"
-                    fill="#fff"
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                />
+            <g data-name="Canvas">
+                <g data-name="Icons / 16px / Clear">
+                    <path
+                        d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"
+                        stroke="#969c9e"
+                        data-name="Vector"
+                        fill="#fff"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    />
+                </g>
             </g>
-            <defs>
-                <clipPath id="icon24pxClearClip0">
-                    <path fill="#fff" d="M0 0h24v24H0z" />
-                </clipPath>
-            </defs>
         </svg>
     );
 }
