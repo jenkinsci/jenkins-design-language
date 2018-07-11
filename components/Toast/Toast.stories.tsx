32a626b3aeb203ad75a61821b4898a21e935ed5f
lausena <lau.sena.gabriel@gmail.com>
2018-07-11 12:43:21 -0400

    initial rendering of toast

--- components/Toast/Toast.stories.tsx
+++ components/Toast/Toast.stories.tsx
@@ -1,12 +1,7 @@
-// TODO refactor based on component
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Toast } from './Toast';
 
-// import * as React from 'react';
-// import { storiesOf } from '@storybook/react';
-// import { Button } from './Button';
-
-// const stories = storiesOf('Button', module);
-//
-// stories.add('should display text', () => {
-//     const story = <Button>some-text</Button>;
-//     return story;
-// });
+storiesOf('Toast', module).add('should display text', () => {
+    return <Toast />;
+});
