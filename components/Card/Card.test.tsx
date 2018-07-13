8b68850be80b02abf8d8704f0674ffa29b6c36e7
lausena <lau.sena.gabriel@gmail.com>
2018-07-13 15:22:45 -0400

    additional test coverage

--- components/Card/Card.test.tsx
+++ components/Card/Card.test.tsx
@@ -1,19 +1,31 @@
 import * as React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow, ReactWrapper } from 'enzyme';
 import { Card } from './Card';
 
 describe('Card', () => {
+    const title = 'My Card';
+    const children = (
+        <div className="child-item">
+            <h5>Sub header</h5>Some content below..
+        </div>
+    );
+    let card: ReactWrapper = mount(shallow(<Card title={title} children={children} />).get(0));
+
     it('should render', () => {
-        const title = 'My Card';
-        const children = (
-            <div className="child-item">
-                <h5>Sub header</h5>Some content below..
-            </div>
-        );
-        const card = mount(<Card title={title} children={children} />);
         const cardHeader = card.find('.Card-Header').text();
         expect(cardHeader).toEqual(title);
         const cardChild = card.find('.child-item h5').text();
         expect(cardChild).toEqual('Sub header');
+    });
+
+    it('should change focus state', () => {
+        expect(card.state()['focused']).toBeFalsy();
+        card.simulate('click');
+        expect(card.state()['focused']).toBeTruthy();
+    });
+
+    it('should render without children', () => {
+        let card: ReactWrapper = mount(shallow(<Card title={title} />).get(0));
+        expect(card.find('.child-item')).toHaveLength(0);
     });
 });
