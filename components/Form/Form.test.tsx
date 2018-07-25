a29f696ae346e780c39cdb991c86241ede3cf336
lausena <lau.sena.gabriel@gmail.com>
2018-07-25 09:53:13 -0400

    Initial files for form and update to base template

---
+++ components/Form/Form.test.tsx
@@ -0,0 +1,10 @@
+// TODO rewrite for your component
+
+// import * as React from 'react';
+// import * as Enzyme from 'enzyme';
+
+describe('Form', () => {
+    it('should ...', () => {
+        expect(true).toBe(true);
+    });
+});
---
+++ components/Form/README.md
@@ -0,0 +1,3 @@
+# Form
+
+This is Form documentation.
