170b2729bdbc24f49e01c0d7e8f448e6df5cfd69
lausena <lau.sena.gabriel@gmail.com>
2018-07-26 11:10:26 -0400

    package updates and initial tests

--- components/Form/Form.test.tsx
+++ components/Form/Form.test.tsx
@@ -1,10 +1,20 @@
-// TODO rewrite for your component
-
-// import * as React from 'react';
-// import * as Enzyme from 'enzyme';
+import * as React from 'react';
+import * as Enzyme from 'enzyme';
 
 describe('Form', () => {
-    it('should ...', () => {
-        expect(true).toBe(true);
+    it('should render an input', () => {
+        const rendered = Enzyme.render(
+            <Form>
+                <Form.Input>
+                    {{
+                        label: 'First Name',
+                        name: 'first-name',
+                        placeholder: 'Enter a first name',
+                        htmlFor: 'first-name',
+                    }}
+                </Form.Input>
+            </Form>
+        );
+        expect(rendered.find('label').text()).toBe('First Name');
     });
 });
