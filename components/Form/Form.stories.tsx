21e5e73818d696ceb9326253f3c8e86dd3d714b1
Keith Zantow <kzantow@gmail.com>
2018-07-31 10:05:57 -0400

    Maybe more like this...

--- components/Form/Form.stories.tsx
+++ components/Form/Form.stories.tsx
@@ -1,10 +1,28 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Form } from './Form';
+import {Form} from './Form';
 
 storiesOf('Form', module).add('should display text', () => {
+    const theUser = {
+        firstName: 'Donald',
+        _lastName: 'Trump',
+        get lastName() {
+            return this._lastName;
+        },
+        set lastName(v: string) {
+            this._lastName = v;
+        }
+    };
     return (
         <>
+            <Form.Vertical>
+                <Form.Item label="First Name">
+                    <Form.InputText placeholder="Enter your first name" value={theUser.firstName} onChange={v => theUser.firstName = v} />
+                </Form.Item>
+                <Form.Item label="Last Name" notifications={[{ type: 'Error', message: 'This was incorrect'}]}>
+                    <Form.InputText placeholder="Enter your first name" value={theUser.lastName} onChange={v => theUser.lastName = v} />
+                </Form.Item>
+            </Form.Vertical>
             <Form>
                 <Form.Header>CloudBees Form</Form.Header>
                 <Form.Input>
--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -5,12 +5,102 @@ export interface ChildProps {
     children: React.ReactNode | JSX.Element;
 }
 
+export class VerticalForm extends React.Component<ChildProps> {
+    render() {
+        return (
+            <div className="Form Form-DisplayVertical">
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+export interface UserNotification {
+    type: 'Success' | 'Info' | 'Warning' | 'Error'; // enum?
+    message: string;
+    actions?: React.ReactNode[];
+}
+
+export interface FormItemProps extends ChildProps {
+    label: string;
+    notifications?: UserNotification[];
+}
+
+export class FormItem extends React.Component<FormItemProps> {
+    render() {
+        const { label, notifications, children } = this.props;
+        return (
+            <>
+                <div className="Form-Label">
+                    {label}
+                </div>
+                {notifications && notifications.map(notification => (
+                    <div className={`Form-Message Form-Message-${notification.type}`}>
+                        <div className="Form-Message-Text">
+                            {notification.message}
+                        </div>
+                        {notification.actions && notification.actions.map(action => (
+                            <div className="Form-Action">
+                                {action}
+                            </div>
+                        ))}
+                    </div>
+                ))}
+                <div className="Form-FormItem">
+                    {children}
+                </div>
+            </>
+        );
+    }
+}
+
+export interface FormInputProps<T> {
+    value: T;
+    onChange: (value: T) => void; // maybe this should be any|boolean and returning false could prevent the change, but validators
+}
+
+export interface FormInputTextProps extends FormInputProps<string> {
+    placeholder?: string;
+}
+
+export interface FormInputState<T> {
+    value: T;
+}
+
+export class FormInputText extends React.Component<FormInputTextProps, FormInputState<string>> {
+    state = { value: this.props.value }; // FIXME should not need state here, it's unclear why just relying on `this.props.value` doesn't work
+    onChange(event: any) { // TODO not 'any'
+        const value = event.target.value;
+        this.props.onChange(value);
+        this.setState({ value });
+    }
+    render() {
+        const { value } = this.state;
+        return (
+            <input
+                {...this.props} // don't do this normally
+                type="text"
+                value={value}
+                onChange={event => this.onChange(event)}
+            />
+        );
+    }
+}
+
+
+
+
 const FormHeader: React.SFC<ChildProps> = ({ children }) => <h4>{children}</h4>;
 
 const initialState = {};
 type State = Readonly<typeof initialState>;
 
 export class Form extends React.Component<{}, State> {
+    public static Vertical = VerticalForm;
+    public static Item = FormItem;
+    public static InputText = FormInputText;
+
+
     public static Header = FormHeader;
     public static Input = FormParts.Input;
     public static Checkbox = FormParts.Checkbox;
