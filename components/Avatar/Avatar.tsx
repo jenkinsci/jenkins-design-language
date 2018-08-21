bd7e34d6615f0b8a9dceea45446ab3477574635c
Mitja Kramberger <mitja.kramberger@gmail.com>
2018-08-21 18:59:14 +0100

    fix: template for components (#208)

--- components/Avatar/Avatar.tsx
+++ components/Avatar/Avatar.tsx
@@ -1,13 +1,8 @@
 import * as React from 'react';
 import { css, CssProps } from '@jdl2/css-util';
 
-export interface AvatarProps extends CssProps {
-}
+export interface AvatarProps extends CssProps {}
 
 export function Avatar(props: AvatarProps) {
-    return (
-        <div className={css('Avatar')}>
-            Avatar
-        </div>
-    );
+    return <div className={css(props, 'Avatar')}>Avatar</div>;
 }
